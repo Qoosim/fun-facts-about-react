e587b11639e6b694562144eda1c6917f092f975d
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-12-23 21:30:30 +0100

    Add navbar and main components to the UI

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav">
+        <div className="logo">
+          <img src="/assets/react-icon-small.png" alt="React-logo" />
+          <h3>ReactFacts</h3>
+        </div>
+        <h4>React Course - Project 1</h4>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
